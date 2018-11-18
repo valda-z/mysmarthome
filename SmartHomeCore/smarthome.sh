07ba192d4006e472aec89c382032bbc704e9abcd
valda-z <valdemar.zavadsky@microsoft.com>
2018-11-18 21:04:44 +0100

    fixed service start on RPi

--- SmartHomeCore/smarthome.sh
+++ SmartHomeCore/smarthome.sh
@@ -27,6 +27,7 @@ d_start()
     # Starting all processes
     cd $GRDIR
     echo -n ", smarthome"
+	stty -F /dev/ttyUSB0 cs8 -parenb -parodd -cmspar cs8 -hupcl -cstopb cread clocal crtscts ignbrk -brkint -ignpar -parmrk -inpck -istrip -inlcr -igncr -icrnl -ixon -ixoff -iuclc -ixany -imaxbel -iutf8 -opost -olcuc -ocrnl -onlcr -onocr -onlret -ofill -ofdel nl0 cr0 tab0 bs0 vt0 ff0 -isig -icanon -iexten -echo -echoe -echok -echonl -noflsh -xcase -tostop -echoprt -echoctl -echoke -flusho -extproc raw speed 9600
     if [ -f $PIDFILE ]; then
         echo -n " already running"
     else
